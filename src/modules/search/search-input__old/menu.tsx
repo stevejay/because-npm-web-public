f74b8e50c3e94119055eaccbb9170065fad27c1a
Steve Johns <steve@stevejay.net>
2018-11-30 23:09:44 +0000

    wip

--- src/modules/search/search-input__old/menu.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import styles from "./menu.css";
-
-const Menu: React.SFC<any> = props => (
-  <ul {...props} className={`${styles.menu}`} ref={props.innerRef} />
-);
-
-export default Menu;
